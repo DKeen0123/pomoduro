22a8299b5ec8e591539fec8efa99fc967f2865af
Daniel Keen <keendaniel01@gmail.com>
2020-09-19 18:02:37 +0100

    Initial commit:

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+// this middleware allows us to use json
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// setup mongoose
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) throw err;
+    console.log('mongodb connection established');
+  }
+);
+
+// setup routes
+app.use('/users', require('./routes/userRouter'));
